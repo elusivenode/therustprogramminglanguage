09bf0e2ef6a0069fa736ed35daec7972ab371cd1
Hamish MacDonald <elusivenode@gmail.com>
2022-09-17 11:02:47 +1000

    hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-cb8f156fc8def340.d
@@ -0,0 +1,5 @@
+/home/hamishaz/Learning/rust/therustprogramminglanguage/hello_cargo/target/debug/deps/hello_cargo-cb8f156fc8def340: src/main.rs
+
+/home/hamishaz/Learning/rust/therustprogramminglanguage/hello_cargo/target/debug/deps/hello_cargo-cb8f156fc8def340.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/hamishaz/Learning/rust/therustprogramminglanguage/hello_cargo/target/debug/hello_cargo: /home/hamishaz/Learning/rust/therustprogramminglanguage/hello_cargo/src/main.rs
