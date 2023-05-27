134af4ce8576c07d37a6cef20c9801ed37cf7bd4
Hamish MacDonald <hamish.macdonald@Hamishs-MBP.modem>
2023-05-27 21:26:00 +1000

    guessing game changes

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -12,5 +17,4 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
-
 }
