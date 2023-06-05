abbd6998d28ad74b8d9975687e062e80dc7154ec
Hamish MacDonald <hamish.macdonald@Hamishs-MacBook-Pro.local>
2023-06-05 16:28:33 +1000

    temp_conv with tests

--- temp_conv/src/temp_conv.rs
+++ temp_conv/src/temp_conv.rs
@@ -15,5 +15,18 @@ mod tests {
     fn test_conv() {
         let result = temp_conv(30.0, "f");
         assert_eq!(((result.0 * 100.0).round() / 100.0, result.1), (-1.11, "C"));
+        let result = temp_conv(95.6, "f");
+        assert_eq!(((result.0 * 100.0).round() / 100.0, result.1), (35.33, "C"));
+        let result = temp_conv(41.0, "c");
+        assert_eq!(((result.0 * 100.0).round() / 100.0, result.1), (105.8, "F"));
+        let result = temp_conv(-1.0, "c");
+        assert_eq!(((result.0 * 100.0).round() / 100.0, result.1), (30.2, "F"));
+        let result = temp_conv(-25.5, "c");
+        assert_eq!(((result.0 * 100.0).round() / 100.0, result.1), (-13.9, "F"));
+        let result = temp_conv(-25.5, "f");
+        assert_eq!(
+            ((result.0 * 100.0).round() / 100.0, result.1),
+            (-31.94, "C")
+        );
     }
 }
