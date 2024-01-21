abb92f8dc158102e034c6b701912ac3f67c61c43
Hamish MacDonald <elusivenode@gmail.com>
2024-01-21 12:05:17 +1000

    fix temp_conv

--- hello_cargo/target/debug/deps/hello_cargo-47c1ef6b6aeceb58.d
+++
@@ -1,5 +0,0 @@
-/home/hamishaz/Learning/rust/therustprogramminglanguage/hello_cargo/target/debug/deps/hello_cargo-47c1ef6b6aeceb58.rmeta: src/main.rs
-
-/home/hamishaz/Learning/rust/therustprogramminglanguage/hello_cargo/target/debug/deps/hello_cargo-47c1ef6b6aeceb58.d: src/main.rs
-
-src/main.rs:
--- temp_conv/src/main.rs
+++ temp_conv/src/main.rs
@@ -3,9 +3,11 @@ use std::env;
 use temp_conv::temp_conv;
 
 fn main() {
+    // to call: ./target/debug/temp_conv 50 C
+    // or:      cargo run 50 c
     let args: Vec<String> = env::args().collect();
-    let temp: f64 = args[2].parse::<f64>().unwrap();
-    let scale: String = args[3].clone();
+    let temp: f64 = args[1].parse::<f64>().unwrap();
+    let scale: String = args[2].clone();
     let (temp, scale) = temp_conv(temp, &scale);
     println!("converted temp is {temp:.2}{scale}");
     let a = (temp * 100.0).round() / 100.0;
