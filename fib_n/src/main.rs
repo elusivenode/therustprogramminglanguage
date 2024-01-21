2e1aa94903d0b7387211e862eb8ff3bd166a9baf
Hamish MacDonald <elusivenode@gmail.com>
2024-01-21 13:22:58 +1000

    fix fib

--- fib_n/src/main.rs
+++ fib_n/src/main.rs
@@ -3,8 +3,9 @@ use fib_n::fib_n;
 use std::env;
 
 fn main() {
+    // call: cargo run 40
     let args: Vec<String> = env::args().collect();
-    let n: u64 = args[2].parse::<u64>().unwrap();
+    let n: u64 = args[1].parse::<u64>().unwrap();
     let fib = fib_n(n);
     println!("The {n}th fibonacci number is {fib}");
 }
