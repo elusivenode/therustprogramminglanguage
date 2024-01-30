dc1322b235c9c11b178c18da5588b9528fc769dc
Hamish MacDonald <elusivenode@gmail.com>
2024-01-30 16:23:30 +1000

    finished integers median mode

--- integers_median_mode/src/main.rs
+++ integers_median_mode/src/main.rs
@@ -11,15 +11,22 @@ fn main() -> io::Result<()> {
     let file = File::open(file_path)?;
     let reader = io::BufReader::new(file);
     let mut integers: HashMap<i32, i32> = HashMap::new();
+    let mut sorted_int_vec: Vec<i32> = Vec::new();
 
     // Iterate over each line in the file
     for line in reader.lines() {
         if let Ok(number) = line?.parse::<i32>() {
-            let ct = integers.entry(number).or_insert(0);
+            sorted_int_vec.push(number);
+            sorted_int_vec.sort(); // for median calc
+            let ct = integers.entry(number).or_insert(0); // for mode calc
             *ct += 1;
         } else {
             eprintln!("Error: Invalid integer format in the file.");
         }
+    }
+
+    for v in sorted_int_vec.iter() {
+        print!("{} ", v);
     }
 
     let mode = get_mode(&integers);
@@ -28,7 +35,24 @@ fn main() -> io::Result<()> {
         println!("  {}", i);
     }
 
+    let median = get_median(&sorted_int_vec);
+    println!("\nmedian value:");
+    println!("  {}", median);
+
     Ok(())
+}
+
+fn get_median(v: &Vec<i32>) -> f64 {
+    let len = v.len();
+    let median;
+    if len % 2 != 0 {
+        let idx = len / 2;
+        median = v[idx].try_into().unwrap();
+    } else {
+        let idx = len / 2;
+        median = ((v[idx - 1] + v[idx]) / 2).try_into().unwrap();
+    }
+    median
 }
 
 fn get_mode(hm: &HashMap<i32, i32>) -> Vec<i32> {
